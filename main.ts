bc53d60a976b514d4533507e995a7a818a7c6512
TahaZaza2 <tahazaza2@outlook.com>
2025-06-15 12:28:15 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+scene.onOverlapTile(SpriteKind.Enemy, assets.tile`transparency16`, function (sprite, location) {
+    sprites.destroy(little_kid)
+})
+let little_kid: Sprite = null
 scene.setBackgroundImage(assets.image`Cloudy Day`)
 // background music, created by Taha Zaza
 music.play(music.stringPlayable("C5 B A G A B C F ", 500), music.PlaybackMode.LoopingInBackground)
-let little_kid = sprites.create(assets.image`lil kid`, SpriteKind.Enemy)
-tiles.setWallAt(tiles.getTileLocation(0, 160), true)
+little_kid = sprites.create(assets.image`lil kid`, SpriteKind.Enemy)
+tiles.setWallAt(tiles.getTileLocation(1, 160), true)
+tiles.setCurrentTilemap(tilemap`level1`)
 let mySprite = sprites.create(assets.image`swords man`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 game.setGameOverEffect(true, effects.confetti)
