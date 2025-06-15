a6bcb2f96b4bfc3bac941f6bfc13e63fa86d6018
TahaZaza2 <tahazaza2@outlook.com>
2025-06-15 13:11:26 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 info.onCountdownEnd(function () {
     game.gameOver(false)
 })
-scene.onOverlapTile(SpriteKind.Enemy, assets.tile`transparency16`, function () {
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(little_kid)
-})
-browserEvents.R.onEvent(browserEvents.KeyEvent.Pressed, function () {
-	
+    game.gameOver(true)
 })
 let little_kid: Sprite = null
 scene.setBackgroundImage(assets.image`Cloudy Day`)
@@ -14,7 +12,7 @@ music.play(music.stringPlayable("C5 B A G A B C F ", 500), music.PlaybackMode.Lo
 little_kid = sprites.create(assets.image`lil kid`, SpriteKind.Enemy)
 tiles.setWallAt(tiles.getTileLocation(1, 160), true)
 tiles.setCurrentTilemap(tilemap`level1`)
-let mySprite: tiles.Location = sprites.create(assets.image`swords man`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`swords man`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 game.setGameOverEffect(true, effects.confetti)
 little_kid.setPosition(randint(1, 160), randint(1, 120))
