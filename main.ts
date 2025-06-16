ef046df1d54f8d423a4e6734d6cc94c491b5c01e
TahaZaza2 <tahazaza2@outlook.com>
2025-06-15 22:10:52 -0400

    updates

--- main.ts
+++ main.ts
@@ -1,19 +1,33 @@
 info.onCountdownEnd(function () {
     game.gameOver(false)
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprites.destroy(little_kid)
+info.onScore(500, function () {
     game.gameOver(true)
+})
+sprites.onDestroyed(SpriteKind.Enemy, function (sprite) {
+    info.setScore(info.score() + 20)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(little_kid, effects.spray, 500)
+    info.stopCountdown()
 })
 let little_kid: Sprite = null
 scene.setBackgroundImage(assets.image`Cloudy Day`)
+info.setScore(0.0001)
 // background music, created by Taha Zaza
 music.play(music.stringPlayable("C5 B A G A B C F ", 500), music.PlaybackMode.LoopingInBackground)
-little_kid = sprites.create(assets.image`lil kid`, SpriteKind.Enemy)
+for (let index = 0; index < 4; index++) {
+    little_kid = sprites.create(assets.image`lil kid1`, SpriteKind.Enemy)
+    little_kid.setVelocity(randint(1, 50), randint(1, 50))
+    little_kid.setPosition(randint(1, 160), randint(1, 120))
+    pause(2000)
+    little_kid = sprites.create(assets.image`lil kid1`, SpriteKind.Enemy)
+    little_kid.setVelocity(randint(1, 50), randint(1, 50))
+    little_kid.setPosition(randint(1, 160), randint(1, 120))
+}
 tiles.setWallAt(tiles.getTileLocation(1, 160), true)
-tiles.setCurrentTilemap(tilemap`level1`)
-let mySprite = sprites.create(assets.image`swords man`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`balloon swords man`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 game.setGameOverEffect(true, effects.confetti)
-little_kid.setPosition(randint(1, 160), randint(1, 120))
-info.startCountdown(20)
+game.setGameOverEffect(false, effects.dissolve)
+info.startCountdown(5)
