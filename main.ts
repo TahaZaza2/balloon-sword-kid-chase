d8c85574725d8a676e57a2c74f5f146b2e597e58
TahaZaza2 <tahazaza2@outlook.com>
2025-06-16 20:24:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let mySprite: Sprite = null
+let little_kid: Sprite = null
+browserEvents.onEvent(browserEvents.Event.Wheel, function () {
+    scene.setBackgroundImage(assets.image`Cloudy Day`)
+    tiles.setWallAt(tiles.getTileLocation(1, 160), true)
+    mySprite = sprites.create(assets.image`balloon swords man`, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 100)
+    info.setScore(game.askForNumber("set your score mister!"))
+    // background music, created by Taha Zaza
+    music.play(music.stringPlayable("C5 B A G A B C F ", 500), music.PlaybackMode.LoopingInBackground)
+    for (let index = 0; index < 4; index++) {
+        little_kid = sprites.create(assets.image`lil kid1`, SpriteKind.Enemy)
+        little_kid.setVelocity(randint(1, 50), randint(1, 50))
+        little_kid.setPosition(randint(1, 160), randint(1, 120))
+    }
+    game.setGameOverEffect(true, effects.confetti)
+    game.setGameOverEffect(false, effects.dissolve)
+    info.startCountdown(10)
+})
 info.onCountdownEnd(function () {
     game.gameOver(false)
 })
@@ -11,23 +30,3 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     sprites.destroy(little_kid, effects.spray, 500)
     info.stopCountdown()
 })
-let little_kid: Sprite = null
-scene.setBackgroundImage(assets.image`Cloudy Day`)
-info.setScore(0.0001)
-// background music, created by Taha Zaza
-music.play(music.stringPlayable("C5 B A G A B C F ", 500), music.PlaybackMode.LoopingInBackground)
-for (let index = 0; index < 4; index++) {
-    little_kid = sprites.create(assets.image`lil kid1`, SpriteKind.Enemy)
-    little_kid.setVelocity(randint(1, 50), randint(1, 50))
-    little_kid.setPosition(randint(1, 160), randint(1, 120))
-    pause(2000)
-    little_kid = sprites.create(assets.image`lil kid1`, SpriteKind.Enemy)
-    little_kid.setVelocity(randint(1, 50), randint(1, 50))
-    little_kid.setPosition(randint(1, 160), randint(1, 120))
-}
-tiles.setWallAt(tiles.getTileLocation(1, 160), true)
-let mySprite = sprites.create(assets.image`balloon swords man`, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
-game.setGameOverEffect(true, effects.confetti)
-game.setGameOverEffect(false, effects.dissolve)
-info.startCountdown(5)
